c7f5b7fa974e8afd5a70c21b0f75053e0b64718a
cesar acosta <ceagdev@gmail.com>
2021-04-12 18:38:54 -0400

    Provide Cubit with OnGenerateRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app/presentation/router/app_router.dart';
 import 'package:flutter_bloc_app/presentation/screens/second_screen.dart';
 import 'package:flutter_bloc_app/presentation/screens/third_screen.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final CounterCubit _counterCubit = CounterCubit();
+  final AppRouter _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
@@ -27,36 +28,14 @@ class _MyAppState extends State<MyApp> {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        routes: {
-          '/': (context) => BlocProvider.value(
-                value: _counterCubit,
-                child: HomeScreen(
-                  title: 'Home Screen',
-                  color: Colors.blueAccent,
-                ),
-              ),
-          '/second': (context) => BlocProvider.value(
-                value: _counterCubit,
-                child: SecondScreen(
-                  title: 'Second Screen',
-                  color: Colors.redAccent,
-                ),
-              ),
-          '/third': (context) => BlocProvider.value(
-                value: _counterCubit,
-                child: ThirdScreen(
-                  title: 'Thrid Screen',
-                  color: Colors.greenAccent,
-                ),
-              ),
-        },
+        onGenerateRoute: _appRouter.onGenerateRoute,
       ),
     );
   }
 
   @override
   void dispose() {
-    _counterCubit.close();
+    _appRouter.dispose();
     super.dispose();
   }
 }
